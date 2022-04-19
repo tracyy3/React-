89d85517206266b7c064eab99c49bf80a115cf4f
tracy <tracyyang336@gmail.com>
2022-04-19 00:39:55 -0400

    courseInfo

--- src/index.js
+++ src/index.js
@@ -2,3 +2,5 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+//Must pass a function in order to test performance in app
